0bcb732597a2ab5b65f30299fd2386ca69f9119f
joymohammod83 <mdjoysarkar83@gmail.com>
2023-10-01 23:59:43 +0600

    last push

--- index.js
+++ index.js
@@ -1,54 +1,54 @@
-// let studage = [18, 19, 20, 22, 21];
-// console.log(studage [1]);
+let studage = [18, 19, 20, 22, 21];
+console.log(studage [1]);
 
-// for (let i = 1; i <= 10; i++){
+for (let i = 1; i <= 10; i++){
 
-//     console.log(i * 10);
-// }
+    console.log(i * 10);
+}
 
-// for (let i = 1; i <= 10; i++){
+for (let i = 1; i <= 10; i++){
 
-//     console.log(i, 'x', 10, '=', (i * 10));
-// }
+    console.log(i, 'x', 10, '=', (i * 10));
+}
 
-// for ( let i=1; i <= 10; i++){
-//     console.log(i, 'x', 8, '=',(i *8) );
+for ( let i=1; i <= 10; i++){
+    console.log(i, 'x', 8, '=',(i *8) );
 
-// }
+}
 
-// function multifly(numm){
-//     for (let i=1; i <=10; i++){
-//         console.log(numm, 'x', i, '=', (i * numm));
-//     }
+function multifly(numm){
+    for (let i=1; i <=10; i++){
+        console.log(numm, 'x', i, '=', (i * numm));
+    }
    
-// }
-// multifly(2)
-// multifly(4)
-// multifly(6)
-// multifly(8)
+}
+multifly(2)
+multifly(4)
+multifly(6)
+multifly(8)
 
-// function namta(multi){
-//     for (let i=1; i <=10; i++){
-//        console.log(multi, 'x', i, '=', (i * multi)); 
-//     }
-// }
+function namta(multi){
+    for (let i=1; i <=10; i++){
+       console.log(multi, 'x', i, '=', (i * multi)); 
+    }
+}
 
-// namta(3)
-// namta(5)
-// namta(7)
-// namta(9)
+namta(3)
+namta(5)
+namta(7)
+namta(9)
 
-// for (let i = 1; i <= 10; i++){
-//     for (let j = 1; j <= 10; j++){
-//         console.log(j, 'x', 10, '=', (i * j));
-//     }
-// }
+for (let i = 1; i <= 10; i++){
+    for (let j = 1; j <= 10; j++){
+        console.log(j, 'x', 10, '=', (i * j));
+    }
+}
 
-// for (let i = 1; i <= 25; i++){
-//     for(let j = 1; j <= 25; j++){
-//         console.log(j, 'x',10, '=', (i * j) );
-//     }
- //}
+for (let i = 1; i <= 25; i++){
+    for(let j = 1; j <= 25; j++){
+        console.log(j, 'x',10, '=', (i * j) );
+    }
+ }
 
  for (let i = 1; i <= 30; i ++){
     if (i < 15){
@@ -77,3 +77,6 @@ console.log(footballer['gk']);
 console.log(footballer['defence']);
 console.log(footballer['midfield']);
 console.log(footballer['forword']);
+
+let homeWork = 'done';
+console.log(homeWork);
